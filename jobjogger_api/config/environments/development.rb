bc732f828f9de2a4b938586c20b08828f8dbeb4f
Yousuf Hassan <yousuf.hassan2021@gmail.com>
2026-03-31 06:52:00 +0000

    Avoid overwriting dev blob host with optional APP_HOST

--- jobjogger_api/config/environments/development.rb
+++ jobjogger_api/config/environments/development.rb
@@ -44,11 +44,6 @@ Rails.application.configure do
     protocol: "http"
   }
 
-  Rails.application.routes.default_url_options = {
-    host: ENV["APP_HOST"],
-    protocol: "https"
-  }
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
--- jobjogger_api/config/environments/production.rb
+++ jobjogger_api/config/environments/production.rb
@@ -57,6 +57,11 @@ Rails.application.configure do
   # Set host to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = { host: "example.com" }
 
+    Rails.application.routes.default_url_options = {
+    host: ENV["APP_HOST"],
+    protocol: "https"
+  }
+
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via bin/rails credentials:edit.
   # config.action_mailer.smtp_settings = {
   #   user_name: Rails.application.credentials.dig(:smtp, :user_name),
