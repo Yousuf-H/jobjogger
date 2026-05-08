127dc3f224419e16959895f6a229b6a2a29e7e08
Yousuf Hassan <yousuf.hassan2021@gmail.com>
2026-05-08 08:43:49 +0000

    Fix FK violation when deleting an organisation with linked contacts

--- jobjogger_api/db/schema.rb
+++ jobjogger_api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_03_000001) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_07_095155) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -202,7 +202,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_03_000001) do
   add_foreign_key "contact_interactions", "contacts"
   add_foreign_key "contact_jobs", "contacts"
   add_foreign_key "contact_jobs", "jobs"
-  add_foreign_key "contacts", "organisations"
+  add_foreign_key "contacts", "organisations", on_delete: :nullify
   add_foreign_key "contacts", "users"
   add_foreign_key "interview_questions", "jobs"
   add_foreign_key "interview_questions", "organisations"
