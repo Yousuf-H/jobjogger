74c92d01fef11c65e9344700363d780462a9259e
Yousuf Hassan <yousuf.hassan2021@gmail.com>
2026-05-30 12:33:30 +0000

    Add Admin::BaseController with 403 guard and admin routes

---
+++ jobjogger_api/app/controllers/api/v1/admin/base_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class Api::V1::Admin::BaseController < Api::V1::AuthenticatedController
+  before_action :require_admin!
+
+  private
+
+  def require_admin!
+    render json: { error: "Forbidden." }, status: :forbidden unless current_user.admin?
+  end
+end
