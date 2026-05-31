3667e6d4b95b8d4b82f5a4bc2dec5435afab8003
Yousuf Hassan <yousuf.hassan2021@gmail.com>
2026-05-31 00:13:02 +0000

    fix: rubocop offense

--- jobjogger_api/config/initializers/sentry.rb
+++ jobjogger_api/config/initializers/sentry.rb
@@ -1,6 +1,6 @@
 Sentry.init do |config|
   config.dsn = ENV["SENTRY_DSN"]
-  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+  config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
   config.traces_sample_rate = Rails.env.production? ? 0.2 : 0.0
   config.send_default_pii = false
   config.enabled_environments = %w[production staging]
